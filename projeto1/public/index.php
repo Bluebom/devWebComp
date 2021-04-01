ddb734cec13460f8ae53f211412e5a133b275c38
Bluebom <franklinhpf@hotmail.com>
2021-04-01 18:22:17 -0300

    esqueleto 1/2

---
+++ projeto1/public/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfólio web para captação de clientes">
+    <meta name="keywords" content="web-developer, web, developer, design-sites, sites, institucionais, sites-institucionais, landpage,land-page">
+    <meta name="author" content="Franklin Henrique">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Franklin Henrique</title>
+</head>
+
+<body>
+    <section class="header-mobile">
+        <span class="menu_hamburguer e">&equiv;</span>
+        <span class="menu_hamburguer x">&times;</span>
+        <header class="mobile w100">
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">Sobre</a></li>
+                    <li><a href="#Services">Serviços</a></li>
+                    <li><a href="#Portfolio">Portfolio</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+    </section>
+    <!--header-mobile-->
+    <section class="header-desktop">
+        <header class="desktop w100">
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#About">Sobre</a></li>
+                    <li><a href="#Services">Serviços</a></li>
+                    <li><a href="#Portfolio">Portfolio</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!--desktop w100-->
+    </section>
+    <!--header-desktop-->
+    <section id="Home">
+        <div class="bg">
+            <h1>Olá, eu sou <span>Franklin Henrique</span></h1>
+            <h2>Sou um desenvolvedor web full-stack</h2>
+            <div class="see_jobs">
+                <p>Veja o meu trabalho <i class="fas fa-arrow-right"></i></p>
+            </div>
+            <!--see_jobs-->
+        </div>
+        <!--bg-->
+    </section>
+    <!--Home-->
+    <section id="About">
+        <h1 class="title">Sobre</h1>
+        <section class="form_hexx">
+            <div class="w33">
+                <div class="hex_about"></div>
+                <h3>Rápido</h3>
+                <p>Tempo de carregamento rápido e livre de travamentos, meu maior foco!</p>
+            </div>
+            <!--w33-->
+            <div class="w33">
+                <div class="hex_about"></div>
+                <h3>Responsivo</h3>
+                <p>Meus sites funcionarão em qualquer dispositivo, pequeno ou grande.</p>
+            </div>
+            <!--w33-->
+            <div class="w33">
+                <div class="hex_about"></div>
+                <h3>Dinâmico</h3>
+                <p>Websites não precisam ser estáticos. Gosto de sites dinâmicos também e comprometo-me a fazê-los.</p>
+            </div>
+            <!--w33-->
+        </section>
+        <!--hexx_about-->
+        <section class="who_i_am">
+            <div class="w50">
+                <img src="" alt="">
+                <h2>Quem é Franklin ?</h2>
+                <p>Eu sou um desenvolvedor web full-stack jr. Tenho uma grande paixão por aprender e pelo que faço, devido a isso estou sempre me atualizando com as inovações do mercado e sempre disposto a adquirir novos conhecimentos.</p>
+                <h3><a href="#Contact">Vamos fazer um website sensacional juntos!</a></h3>
+            </div>
+            <!--w50-->
+            <div class="w50 skills">
+                <h2>Tecnologias</h2>
+                <!-- aqui vai as barras de habilidades -->
+            </div>
+            <!--w50-->
+        </section><!-- who_i_am -->
+    </section>
+    <!--About-->
+    <section id="Services">
+        <section class="form_rect">
+            <div class="w25">
+                <i class="fas fa-chart-pie"></i>
+                <p>Obtenha insights sobre quem está navegando em seu site para que assim possa tomar decisões de negócio mais eficazes e inteligentes.</p>
+                <button class="btn">Entre em contato já!</button>
+            </div><!-- w25 -->
+            <div class="w25">
+                <i class="fas fa-chart-line"></i>
+                <p>Procurando melhorar a performance do seu site, SEO, ou a experiência do usuário? </p>
+                <button class="btn">Entre em contato já!</button>
+            </div><!-- w25 -->
+            <div class="w25">
+                <i class="fas fa-pen-nib"></i>
+                <p> Projetos modernos, otimizádos, performáticos e limpos. Eficazes na hora de converter simples visitantes em clientes!</p>
+                <button class="btn">Entre em contato já!</button>
+            </div><!-- w25 -->
+            <div class="w25">
+                <i class="far fa-calendar-check"></i>
+                <p>Comprometimento em entregar os projetos no prazo correto, sem atrasos e nem aumento de valores.</p>
+                <button class="btn">Entre em contato já!</button>
+            </div><!-- w25 -->
+        </section><!-- form_rect -->
+    </section><!-- Services -->
+    <section id="Portfolio">
+        <div class="down_triangle"></div>
+        <section class="carousel_JS">
+            <!-- Aqui vai o carrocel -->
+        </section>
+    </section><!-- Portfolio -->
+
+
+    <script src="../assets/js/script.js"></script>
+</body>
+
+</html>
