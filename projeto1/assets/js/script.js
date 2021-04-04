86043e29a0aefbed8fff2831718d4291b79d778e
Bluebom <franklinhpf@hotmail.com>
2021-04-04 11:47:06 -0300

    parte lógica 2/7

--- projeto1/assets/js/script.js
+++ projeto1/assets/js/script.js
@@ -1,4 +1,14 @@
 const progress = document.querySelectorAll('.skill_lv');
+const openBtn = document.querySelector('span.e')
+const closeBtn = document.querySelector('span.x');
+const boxMobi = document.querySelector('header.mobile');
+const mobiul = document.querySelectorAll('.mobile ul li a');
+const deskul = document.querySelectorAll('.desktop ul li a');
+// span.x,
+// header.mobile
+
+let mobimarked = mobiul[0];
+let dekmarked = deskul[0];
 
 setTimeout(() => {
     progress.forEach((value, i) => {
@@ -7,4 +17,54 @@ setTimeout(() => {
     })
 }, 1000)
 
+function chMarked(previouM, nextM) {
+    previouM.classList.remove("marked");
+    nextM.classList.add("marked");
+}
 
+function openBoxMobi(openbtn, closebtn, boxmobi){
+    openbtn.style.display = 'none';
+    closebtn.style.display = 'inline';
+    boxMobi.style.display = 'block';
+}
+
+function closeBoxMobi(openbtn, closebtn, boxmobi){
+    openbtn.style.display = 'inline';
+    closebtn.style.display = 'none';
+    boxMobi.style.display = 'none';
+}
+
+document.addEventListener('click', (e) =>{
+    const et = e.target;
+    
+    if(et == openBtn) {
+        openBoxMobi(openBtn, closeBtn, boxMobi);
+    }
+
+    if(et == closeBtn) {
+        closeBoxMobi(openBtn, closeBtn, boxMobi);
+    }
+
+    deskul.forEach((value, i) =>{
+        if(et == value){
+            if(et.classList.contains('marked')){
+                dekmarked = et;
+            }else{
+                chMarked(dekmarked, et)
+                dekmarked = et;
+                closeBoxMobi(et, closeBtn, boxMobi);
+            }
+        }
+    })
+    mobiul.forEach((value, i) =>{
+        if(et == value){
+            if(et.classList.contains('marked')){
+                mobimarked = et;
+            }else{
+                chMarked(mobimarked, et)
+                mobimarked = et;
+                closeBoxMobi(openBtn, closeBtn, boxMobi);
+            }
+        }
+    })
+})
--- projeto1/public/index.php
+++ projeto1/public/index.php
@@ -7,7 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Portfólio web para captação de clientes">
-    <meta name="keywords" content="web-developer, web, developer, design-sites, sites, institucionais, sites-institucionais, landpage,land-page">
+    <meta name="keywords"
+        content="web-developer, web, developer, design-sites, sites, institucionais, sites-institucionais, landpage,land-page">
     <meta name="author" content="Franklin Henrique">
     <title>Franklin Henrique</title>
 </head>
@@ -19,11 +20,11 @@
         <header class="mobile w100">
             <nav>
                 <ul>
-                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Home" class="marked">Home</a></li>
                     <li><a href="#About">Sobre</a></li>
                     <li><a href="#Services">Serviços</a></li>
                     <li><a href="#Portfolio">Portfolio</a></li>
-                    <li><a href="#">Contato</a></li>
+                    <li><a href="#Contact">Contato</a></li>
                 </ul>
             </nav>
         </header>
@@ -33,20 +34,20 @@
         <header class="desktop w100">
             <nav>
                 <ul>
-                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Home" class="marked">Home</a></li>
                     <li><a href="#About">Sobre</a></li>
                     <li><a href="#Services">Serviços</a></li>
                     <li><a href="#Portfolio">Portfolio</a></li>
-                    <li><a href="#">Contato</a></li>
+                    <li><a href="#Contact">Contato</a></li>
                 </ul>
             </nav>
         </header>
         <!--desktop w100-->
     </section>
     <!--header-desktop-->
-    <section id="Home">
+    <section id="Home" class="all_vh">
         <div class="bg">
-            <h1>Olá, eu sou <span>Franklin Henrique</span></h1>
+            <h1>Olá, eu sou <span>Franklin Henrique</span>.</h1>
             <h2>Sou um desenvolvedor web full-stack</h2>
             <div class="see_jobs">
                 <p>Veja o meu trabalho <i class="fas fa-arrow-right"></i></p>
@@ -57,51 +58,122 @@
     </section>
     <!--Home-->
     <div class="center">
-        <section id="About">
-            <h1 class="title">Sobre</h1>
+        <section id="About" class="all_vh">
+            <div class="title">
+                <h1>Sobre</h1>
+            </div><!-- title -->
             <section class="form_hexx">
                 <div class="w33">
-                    <div class="hex_about"></div>
+                    <div class="hex_about_1"></div>
                     <h3>Rápido</h3>
                     <p>Tempo de carregamento rápido e livre de travamentos, meu maior foco!</p>
                 </div>
                 <!--w33-->
                 <div class="w33">
-                    <div class="hex_about"></div>
+                    <div class="hex_about_2"></div>
                     <h3>Responsivo</h3>
                     <p>Meus sites funcionarão em qualquer dispositivo, pequeno ou grande.</p>
                 </div>
                 <!--w33-->
                 <div class="w33">
-                    <div class="hex_about"></div>
+                    <div class="hex_about_3"></div>
                     <h3>Dinâmico</h3>
-                    <p>Websites não precisam ser estáticos. Gosto de sites dinâmicos também e comprometo-me a fazê-los.</p>
+                    <p>Websites não precisam ser estáticos. Gosto de sites dinâmicos também e comprometo-me a fazê-los.
+                    </p>
                 </div>
                 <!--w33-->
             </section>
             <!--hexx_about-->
-            <section class="who_i_am">
-                <div class="w50">
-                    <img src="" alt="">
+            <section class="who_i_am" >
+                <div class="w50 description">
+                    <img src="../assets/image/hexx 1Me.png" alt="uma foto minha">
                     <h2>Quem é Franklin ?</h2>
-                    <p>Eu sou um desenvolvedor web full-stack jr. Tenho uma grande paixão por aprender e pelo que faço, devido a isso estou sempre me atualizando com as inovações do mercado e sempre disposto a adquirir novos conhecimentos.</p>
+                    <p>Eu sou um desenvolvedor web full-stack jr. Tenho uma grande paixão por aprender e pelo que faço,
+                        devido a isso estou sempre me atualizando com as inovações do mercado e sempre disposto a
+                        adquirir novos conhecimentos.</p>
                     <h3><a href="#Contact">Vamos fazer um website sensacional juntos!</a></h3>
                 </div>
                 <!--w50-->
                 <div class="w50 skills">
                     <h2>Tecnologias</h2>
-                    <!-- aqui vai as barras de habilidades -->
+                    <div class="skillBox">
+                        <span>HTML5</span>
+                        <div class="skill">
+                            <div class="skill_lv w90" completed="90"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>CSS3</span>
+                        <div class="skill">
+                            <div class="skill_lv w90" completed="90"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>JavaScript</span>
+                        <div class="skill">
+                            <div class="skill_lv w75" completed="75"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>JQuery</span>
+                        <div class="skill">
+                            <div class="skill_lv w50" completed="55"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>PHP</span>
+                        <div class="skill">
+                            <div class="skill_lv w75" completed="65"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>MySQL</span>
+                        <div class="skill">
+                            <div class="skill_lv w75" completed="65"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>GIT</span>
+                        <div class="skill">
+                            <div class="skill_lv w75" completed="75"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
+                    <div class="skillBox">
+                        <span>Linux</span>
+                        <div class="skill">
+                            <div class="skill_lv w90" completed="75"></div>
+                        </div>
+                        <!-- skill -->
+                    </div>
+                    <!-- skillBox -->
                 </div>
                 <!--w50-->
             </section><!-- who_i_am -->
         </section>
         <!--About-->
-        <section id="Services">
-            <h1 class="title">Serviços</h1>
+        <section id="Services" class="all_vh" >
+            <div class="title">
+                <h1>Serviços</h1>
+            </div><!-- title -->
             <section class="form_rect">
                 <div class="w25">
                     <i class="fas fa-chart-pie"></i>
-                    <p>Obtenha insights sobre quem está navegando em seu site para que assim possa tomar decisões de negócio mais eficazes e inteligentes.</p>
+                    <p>Obtenha insights sobre quem está navegando em seu site para que assim possa tomar decisões de
+                        negócio mais eficazes e inteligentes.</p>
                     <button class="btn">Entre em contato já!</button>
                 </div><!-- w25 -->
                 <div class="w25">
@@ -111,49 +183,58 @@
                 </div><!-- w25 -->
                 <div class="w25">
                     <i class="fas fa-pen-nib"></i>
-                    <p> Projetos modernos, otimizádos, performáticos e limpos. Eficazes na hora de converter simples visitantes em clientes!</p>
+                    <p> Projetos modernos, otimizádos, performáticos e limpos. Eficazes na hora de converter simples
+                        visitantes em clientes!</p>
                     <button class="btn">Entre em contato já!</button>
                 </div><!-- w25 -->
                 <div class="w25">
                     <i class="far fa-calendar-check"></i>
-                    <p>Comprometimento em entregar os projetos no prazo correto, sem atrasos e nem aumento de valores.</p>
+                    <p>Comprometimento em entregar os projetos no prazo correto, sem atrasos e nem aumento de valores.
+                    </p>
                     <button class="btn">Entre em contato já!</button>
                 </div><!-- w25 -->
             </section><!-- form_rect -->
         </section><!-- Services -->
-        <section id="Portfolio">
+        <section id="Portfolio" class="all_vh">
             <div class="down_triangle"></div>
             <div class="portfolio_carousel">
-                <h1 class="title">Portfólio</h1>
+                <div class="title">
+                    <h1>Portfólio</h1>
+                </div><!-- title -->
                 <section class="carousel_JS">
                     <!-- Aqui vai o carrocel -->
                 </section><!-- carousel_JS -->
             </div><!-- portfolio_carousel -->
         </section><!-- Portfolio -->
-        <section id="Contact">
+        <section id="Contact" class="all_vh">
             <div class="form_contact">
-                <h1 class="title">Contato</h1>
+                <div class="title">
+                    <h1>Contato</h1>
+                </div><!-- title -->
                 <form method="post">
-                    <p class="w100"><i class="fas fa-user-alt"></i><input placeholder="Nome*" type="text" name="nome" id="Name" required></p>
-                    <p class="w100"><i class="fas fa-envelope"></i><input placeholder="Email*" type="email" name="email" id="Email" required></p>
-                    <p class="w100"><textarea name="msg" id="Msg" cols="30" rows="10" placeholder="Deixe sua mensagem aqui!"></textarea></p>
+                    <p class="w100"><i class="fas fa-user-alt"></i><input placeholder="Nome*" type="text" name="nome"
+                            id="Name" required></p>
+                    <p class="w100"><i class="fas fa-envelope"></i><input placeholder="Email*" type="email" name="email"
+                            id="Email" required></p>
+                    <p class="w100"><textarea name="msg" id="Msg" cols="30" rows="10"
+                            placeholder="Deixe sua mensagem aqui!"></textarea></p>
                     <p class="w100"><input type="submit" value="Enviar" name="acao"></p>
                 </form>
             </div><!-- form_contact -->
         </section><!-- contact -->
+        <span><button><i class="fas fa-arrow-circle-up"></i></button></span>
+        <footer>
+            <div class="form_social">
+                <span><i class="fab fa-linkedin-in"></i></span>
+                <span><i class="fab fa-github"></i></span>
+                <span><i class="fab fa-youtube"></i></span>
+            </div><!-- form_social -->
+            <div class="copyright">
+                <p>Franklin Henrique <span>&copy;2021</span></p>
+            </div>
+        </footer>
     </div>
     <!-- center -->
-    <footer>
-        <span><button><i class="fas fa-arrow-circle-up"></i></button></span>
-        <div class="form_social">
-            <span><i class="fab fa-linkedin-in"></i></span>
-            <span><i class="fab fa-github"></i></span>
-            <span><i class="fab fa-youtube"></i></span>
-        </div><!-- form_social -->
-        <div class="copyright">
-            <p>Franklin Henrique <span>&copy;2021</span></p>
-        </div>
-    </footer>
 
 
     <script src="../assets/js/script.js"></script>
