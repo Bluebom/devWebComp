f8f2b853e5d5cc6ffe44343c8a3dfab374b65084
Bluebom <franklinhpf@hotmail.com>
2021-04-19 13:50:03 -0300

    fixing

--- projeto1/ajax/formularios.php
+++ projeto1/ajax/formularios.php
@@ -17,10 +17,10 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'mail.franklinhenrique.com';  // Specify main and backup SMTP servers
+$mail->Host = '';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'franklin@franklinhenrique.com';                 // SMTP username
-$mail->Password = 'franklinjudo123';                           // SMTP password
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
