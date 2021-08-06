9b2c6628db70f5195604f6c695abfc531b3fba21
Bluebom <franklinhpf@hotmail.com>
2021-08-05 22:44:51 -0300

    hoje nao deu pra estudar controllers

---
+++ mvc/mvc2/app/Controller/pages/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller\Pages;
+
+/**
+ * Método responsável por retornar o conteúdo (view) da nossa home
+ * @return string
+ */
+class Home
+{
+  public static function getHome()
+  {
+    return 'Olá Mundo!';
+  }
+}
